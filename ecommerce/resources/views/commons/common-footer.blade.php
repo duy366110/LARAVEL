2820c428f542c71253c7ab8fa133abc148334ff6
duy366110 <duy366110@gmail.com>
2024-06-27 15:22:36 +0700

    #Config template footer v-02.

--- ecommerce/resources/views/commons/common-footer.blade.php
+++ ecommerce/resources/views/commons/common-footer.blade.php
@@ -13,7 +13,7 @@
                     <a class="hover:opacity-60" href="#">
                         <img class="w-[170px]" src="{{asset("assets/images/icons/white-CHplay.png")}}" alt="icon flatform"/>
                     </a>
-                    
+
                     <a class="hover:opacity-60" href="#">
                         <img class="w-[170px] my-2" src="{{asset("assets/images/icons/white-Applestore.png")}}" alt="icon flatform"/>
                     </a>
@@ -27,122 +27,140 @@
                 <ul class="grid grid-cols-4 gap-4">
                     <li>
                         <div>
-                            <h2>Công ty</h2>
-                            <ul>
-                                <li><a href="#">Về chúng tôi</a></li>
-                                <li><a href="#">Đối tác</a></li>
-                                <li><a href="#">Tại sao chọn Tanca</a></li>
-                                <li><a href="#">Báo chí</a></li>
-                                <li><a href="#">Nghề nghiệp</a></li>
+                            <h2 class="mb-6 font-extrabold text-xl/[30px] uppercase leading-7 text-[#FF8D23]">Công ty</h2>
+                            <ul class="mb-4">
+                                <li class="mb-4"><a href="#">Về chúng tôi</a></li>
+                                <li class="mb-4"><a href="#">Đối tác</a></li>
+                                <li class="mb-4"><a href="#">Tại sao chọn Tanca</a></li>
+                                <li class="mb-4"><a href="#">Báo chí</a></li>
+                                <li class="mb-4"><a href="#">Nghề nghiệp</a></li>
                             </ul>
                         </div>
+
                         <div>
-                            <h2>Sản phẩm</h2>
-                            <ul>
-                                <li><a href="#">Giá cả</a></li>
-                                <li><a href="#">Thiết bị</a></li>
+                            <h2 class="mb-6 font-extrabold text-xl/[30px] uppercase leading-7 text-[#FF8D23]">Sản phẩm</h2>
+                            <ul class="mb-4">
+                                <li class="mb-4"><a href="#">Giá cả</a></li>
+                                <li class="mb-4"><a href="#">Thiết bị</a></li>
                             </ul>
                         </div>
+
                         <div>
-                            <h2>Hỗ trợ</h2>
-                            <ul>
-                                <li><a href="#">GDPR</a></li>
-                                <li><a href="#">Tanca API</a></li>
-                                <li><a href="#">Trung tâm trợ giúp</a></li>
+                            <h2 class="mb-6 font-extrabold text-xl/[30px] uppercase leading-7 text-[#FF8D23]">Hỗ trợ</h2>
+                            <ul class="mb-4">
+                                <li class="mb-4"><a href="#">GDPR</a></li>
+                                <li class="mb-4"><a href="#">Tanca API</a></li>
+                                <li class="mb-4"><a href="#">Trung tâm trợ giúp</a></li>
                             </ul>
                         </div>
                     </li>
 
                     <li>
                         <div>
-                            <h2>Tính năng</h2>
-                            <ul>
-                                <li><a href="#">Tìm kiếm nhân tài</a></li>
-                                <li><a href="#">Đào tạo trực tuyến</a></li>
-                                <li><a href="#">Quản lý nguồn lực</a></li>
-                                <li><a href="#">Xếp ca thông minh</a></li>
-                                <li><a href="#">Quản lý công phép</a></li>
-                                <li><a href="#">Hệ thống lương</a></li>
-                                <li><a href="#">Quản lý gắn kết</a></li>
-                                <li><a href="#">Quản lý nhân tài</a></li>
-                                <li><a href="#">Hoạch định nhân lực</a></li>
-                                <li><a href="#">Quản lý yêu cầu</a></li>
-                                <li><a href="#">Giao việc & quy trình</a></li>
-                                <li><a href="#">Quản lý hiệu suất</a></li>
+                            <h2 class="mb-6 font-extrabold text-xl/[30px] uppercase leading-7 text-[#FF8D23]">Tính năng</h2>
+                            <ul class="mb-4">
+                                <li class="mb-4"><a href="#">Tìm kiếm nhân tài</a></li>
+                                <li class="mb-4"><a href="#">Đào tạo trực tuyến</a></li>
+                                <li class="mb-4"><a href="#">Quản lý nguồn lực</a></li>
+                                <li class="mb-4"><a href="#">Xếp ca thông minh</a></li>
+                                <li class="mb-4"><a href="#">Quản lý công phép</a></li>
+                                <li class="mb-4"><a href="#">Hệ thống lương</a></li>
+                                <li class="mb-4"><a href="#">Quản lý gắn kết</a></li>
+                                <li class="mb-4"><a href="#">Quản lý nhân tài</a></li>
+                                <li class="mb-4"><a href="#">Hoạch định nhân lực</a></li>
+                                <li class="mb-4"><a href="#">Quản lý yêu cầu</a></li>
+                                <li class="mb-4"><a href="#">Giao việc & quy trình</a></li>
+                                <li class="mb-4"><a href="#">Quản lý hiệu suất</a></li>
                             </ul>
                         </div>
                     </li>
 
                     <li>
                         <div>
-                            <h2>Giải pháp</h2>
-                            <ul>
-                                <li><a href="#">Nhà hàng/Cafe</a></li>
-                                <li><a href="#">Phần mềm</a></li>
-                                <li><a href="#">Bán lẻ</a></li>
-                                <li><a href="#">Xây dựng</a></li>
-                                <li><a href="#">Sản xuất</a></li>
-                                <li><a href="#">Dịch vụ vệ sinh</a></li>
-                                <li><a href="#">Khách sạn/Dịc vụ lưu trú</a></li>
-                                <li><a href="#">Chăm sóc sức khoẻ</a></li>
-                                <li><a href="#">Giáo dục</a></li>
-                                <li><a href="#">Siêu thị</a></li>
-                                <li><a href="#">Nhà chăm sóc</a></li>
-                                <li><a href="#">Nông nghiệp</a></li>
-                                <li><a href="#">Logistics</a></li>
-                                <li><a href="#">Dịch vụ du lịch</a></li>
-                                <li><a href="#">Dịch vụ ý tế</a></li>
+                            <h2 class="mb-6 font-extrabold text-xl/[30px] uppercase leading-7 text-[#FF8D23]">Giải pháp</h2>
+                            <ul class="mb-4">
+                                <li class="mb-4"><a href="#">Nhà hàng/Cafe</a></li>
+                                <li class="mb-4"><a href="#">Phần mềm</a></li>
+                                <li class="mb-4"><a href="#">Bán lẻ</a></li>
+                                <li class="mb-4"><a href="#">Xây dựng</a></li>
+                                <li class="mb-4"><a href="#">Sản xuất</a></li>
+                                <li class="mb-4"><a href="#">Dịch vụ vệ sinh</a></li>
+                                <li class="mb-4"><a href="#">Khách sạn/Dịc vụ lưu trú</a></li>
+                                <li class="mb-4"><a href="#">Chăm sóc sức khoẻ</a></li>
+                                <li class="mb-4"><a href="#">Giáo dục</a></li>
+                                <li class="mb-4"><a href="#">Siêu thị</a></li>
+                                <li class="mb-4"><a href="#">Nhà chăm sóc</a></li>
+                                <li class="mb-4"><a href="#">Nông nghiệp</a></li>
+                                <li class="mb-4"><a href="#">Logistics</a></li>
+                                <li class="mb-4"><a href="#">Dịch vụ du lịch</a></li>
+                                <li class="mb-4"><a href="#">Dịch vụ ý tế</a></li>
                             </ul>
                         </div>
 
                         <div>
-                            <h2>Quy mô kinh doanh</h2>
-                            <ul>
-                                <li><a href="#">Doanh nghiệp lớn</a></li>
-                                <li><a href="#">Doanh nghiệp vừa và nhỏ</a></li>
+                            <h2 class="mb-6 font-extrabold text-xl/[30px] uppercase leading-7 text-[#FF8D23]">Quy mô kinh doanh</h2>
+                            <ul class="mb-4">
+                                <li class="mb-4"><a href="#">Doanh nghiệp lớn</a></li>
+                                <li class="mb-4"><a href="#">Doanh nghiệp vừa và nhỏ</a></li>
                             </ul>
                         </div>
                     </li>
 
                     <li>
                         <div>
-                            <h2>Tài nguyên</h2>
-                            <ul>
-                                <li><a href="#">Blog</a></li>
-                                <li><a href="#">Câu chuyện của khách hàng</a></li>
-                                <li><a href="#">Báo cáo</a></li>
-                                <li><a href="#">Trung tâm tài liệu</a></li>
-                                <li><a href="#">Thiết bị</a></li>
-                                <li><a href="#">Camera trí tuệ nhân tạo</a></li>
-                                <li><a href="#">Youtube</a></li>
-                                <li><a href="#">Fanpage</a></li>
-                                <li><a href="#">Tiktok</a></li>
+                            <h2 class="mb-6 font-extrabold text-xl/[30px] uppercase leading-7 text-[#FF8D23]">Tài nguyên</h2>
+                            <ul class="mb-4">
+                                <li class="mb-4"><a href="#">Blog</a></li>
+                                <li class="mb-4"><a href="#">Câu chuyện của khách hàng</a></li>
+                                <li class="mb-4"><a href="#">Báo cáo</a></li>
+                                <li class="mb-4"><a href="#">Trung tâm tài liệu</a></li>
+                                <li class="mb-4"><a href="#">Thiết bị</a></li>
+                                <li class="mb-4"><a href="#">Camera trí tuệ nhân tạo</a></li>
+                                <li class="mb-4"><a href="#">Youtube</a></li>
+                                <li class="mb-4"><a href="#">Fanpage</a></li>
+                                <li class="mb-4"><a href="#">Tiktok</a></li>
                             </ul>
                         </div>
 
                         <div>
-                            <h2>Liên hệ</h2>
+                            <h2 class="mb-6 font-extrabold text-xl/[30px] uppercase leading-7 text-[#FF8D23]">Liên hệ</h2>
                             <div>
                                 <h3>Tanca Vietnam</h3>
                                 <ul>
-                                    <li><a href="#"> 0899.1999.54</a></li>
-                                    <li><a href="#">0965.73.53.03 (Zalo)</a></li>
+                                    <li class="mb-4 flex">
+                                        <span class="phone-contact relative">
+                                            <i class="fa fa-phone" aria-hidden="true"></i>
+                                        <span>
+                                        <a href="#"> 0899.1999.54</a>
+                                    </li>
+
+                                    <li class="mb-4 flex">
+                                        <span class="phone-contact">
+                                            <i class="fa fa-phone" aria-hidden="true"></i>
+                                        <span>
+                                        <a href="#">0965.73.53.03 (Zalo)</a>
+                                    </li>
                                 </ul>
                             </div>
 
                             <div>
                                 <h3>Tanca USA</h3>
                                 <ul>
-                                    <li><a href="#"> 1-855-633-2443</a></li>
+                                    <li class="mb-4 flex">
+                                        <span class="phone-contact">
+                                            <i class="fa fa-phone" aria-hidden="true"></i>
+                                        <span>
+                                        <a href="#"> 1-855-633-2443</a>
+                                    </li>
                                 </ul>
                             </div>
                         </div>
 
                         <div>
-                            <h2>Nhắn tin whatapp</h2>
-                            <ul>
-                                <li><a href="#">support@tanca.io</a></li>
-                                <li><a href="#">partner@tanca.io</a></li>
+                            <h2 class="mb-6 font-extrabold text-xl/[30px] uppercase leading-7 text-[#FFE459]">Nhắn tin whatapp</h2>
+                            <ul class="mb-4">
+                                <li class="mb-4"><a href="#">support@tanca.io</a></li>
+                                <li class="mb-4"><a href="#">partner@tanca.io</a></li>
                             </ul>
                         </div>
                     </li>
