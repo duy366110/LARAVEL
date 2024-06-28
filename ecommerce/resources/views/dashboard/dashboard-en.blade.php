a7c182d336fc5f50eb0685a23df133daccb5b45e
duy366110 <duy366110@gmail.com>
2024-06-28 13:36:10 +0700

    #Config template tabs section feature.

--- ecommerce/resources/views/dashboard/dashboard-en.blade.php
+++ ecommerce/resources/views/dashboard/dashboard-en.blade.php
@@ -67,6 +67,55 @@
         </div>
     </section>
 
+    <!-- SECTION FEATURE -->
+    <section class="main-feature-section max-w-screen-2xl m-auto">
+        <div class="container m-auto pt-[120px]">
+            <h2 class="font-bold text-3xl text-center">Thành công của doanh nghiệp phụ thuộc vào chiến lược nhân sự</h2>
+            <h3 class="text-center mt-6 text-[18px]">Tanca hỗ trợ doanh nghiệp theo dõi toàn diện sự phát triển sự nghiệp của nhân viên.</h3>
+
+            <div class="feature-wrapper">
+                <ul class="feature-tabs border-b border-gray-200 mt-20 flex flex-wrap gap-5 justify-center">
+                    <li class="feature-tab-item">
+                        <button class="inline-flex items-center justify-center p-2 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 group cursor-pointer font-medium text-sm md:text-lg mx-0 md:mx-0 border-transparent">
+                            <img src="{{asset("assets/images/icons/icon-tab-talent-aquistion.svg")}}" alt="tanca icon" />
+                            <span>Tìm kiếm nhân tài</span>
+                        </button>
+                    </li>
+
+                    <li class="feature-tab-item">
+                        <button class="inline-flex items-center justify-center p-2 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 group cursor-pointer font-medium text-sm md:text-lg mx-0 md:mx-0 border-transparent">
+                            <img src="{{asset("assets/images/icons/icon-tab-benefits.svg")}}" alt="tanca icon" />
+                            <span>Quyền lợi nhân viên</span>
+                        </button>
+                    </li>
+
+                    <li class="feature-tab-item">
+                        <button class="inline-flex items-center justify-center p-2 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 group cursor-pointer font-medium text-sm md:text-lg mx-0 md:mx-0 border-transparent">
+                            <img src="{{asset("assets/images/icons/icon-tab-talent-management.svg")}}" alt="tanca icon" />
+                            <span>Quản lý nhân tài</span>
+                        </button>
+                    </li>
+
+                    <li class="feature-tab-item">
+                        <button class="inline-flex items-center justify-center p-2 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 group cursor-pointer font-medium text-sm md:text-lg mx-0 md:mx-0 border-transparent">
+                            <img src="{{asset("assets/images/icons/icon-tab-performance.svg")}}" alt="tanca icon" />
+                            <span>Thúc đẩy hiệu suất</span>
+                        </button>
+                    </li>
+
+                    <li class="feature-tab-item">
+                        <button class="inline-flex items-center justify-center p-2 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 group cursor-pointer font-medium text-sm md:text-lg mx-0 md:mx-0 border-transparent">
+                            <img src="{{asset("assets/images/icons/icon-tab-workforce.svg")}}" alt="tanca icon" />
+                            <span>Quản lý nguồn lực</span>
+                        </button>
+                    </li>
+
+                </ul>
+                <div class="feature-contents py-20">Feature content</div>
+            </div>
+        </div>
+    </section>
+
     <!-- SECTION SOLUTION -->
     <section class="bg-white">
         <div class="max-w-screen-xl 2xl:max-w-screen-2xl m-auto py-24">
